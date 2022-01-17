58835d4ab8bf40c32b5e61d030b628c83d425fd9
ji-myeung-Kim <wlaud5314@naver.com>
2022-01-17 22:36:04 +0900

    [동적 쿼리와 성능 최적화]

---
+++ src/main/java/study/querydsl/dto/MemberTeamDto.java
@@ -0,0 +1,27 @@
+package study.querydsl.dto;
+
+import com.querydsl.core.annotations.QueryProjection;
+import lombok.Data;
+
+@Data
+public class MemberTeamDto {
+
+    private Long memberId;
+    private String username;
+    private int age;
+    private Long teamId;
+    private String teamName;
+
+    /**
+     *
+     * 순수 Dto가 아니라 QueryDSl 의존자체가 단점
+     */
+    @QueryProjection
+    public MemberTeamDto(Long memberId, String username, int age, Long teamId, String teamName) {
+        this.memberId = memberId;
+        this.username = username;
+        this.age = age;
+        this.teamId = teamId;
+        this.teamName = teamName;
+    }
+}
